63ae76af8903208ee955f11579f0f31c474dae8c
QuietMonkey <taillandierbertrand@gmail.com>
2019-09-09 16:45:36 +0200

    App is now a Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
 
-
-
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
