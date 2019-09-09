da6d1b85a1dbb74837e02a34c381029e0c15bc7a
QuietMonkey <taillandierbertrand@gmail.com>
2019-09-09 18:46:19 +0200

    create first cards for charts && variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
 
+import ChartistGraph from 'react-chartist'
+
+import { emailsSubscriptionChart, completedTasksChart } from "./variables/charts.js"
 import './App.css'
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
+        <div className='cardsChart'>
 
+          <div className='card one'>
+            <ChartistGraph
+              className="ct-chart"
+              data={emailsSubscriptionChart.data}
+              type="Bar"
+              options={emailsSubscriptionChart.options}
+              responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+              listener={emailsSubscriptionChart.animation}
+            />
+          </div>
+
+          <div className='card two'>
+            <ChartistGraph
+              className="ct-chart"
+              data={completedTasksChart.data}
+              type="Line"
+              options={completedTasksChart.options}
+              listener={completedTasksChart.animation}
+            />
+          </div>
+
+          <div className='card three'>
+         
+          </div>
+
+        </div>
       </div>
     )
   }
